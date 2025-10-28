1cf7f2e553005b2e45f1abdf93a12d21d992945b
ClaraXinn <claraxin8@gmail.com>
2025-10-29 01:45:59 +0800

    navbar init

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import Hello from "./components/hello";
+
+
 
 export default function Home() {
   console.log("What am I? -- SERVER/CLIENT")
   return (
     <>
-      <h1 className="text-3xl">Welcome to SEO website</h1>
-      <Hello/>
+      HOME
+
 
     </>
 
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>___________________</div>
+  )
+}
+
+export default loading
