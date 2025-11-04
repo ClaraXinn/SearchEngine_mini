3b68dd871655637feb73bf612fbbfbeabe77e027
ClaraXinn <claraxin8@gmail.com>
2025-11-05 04:14:06 +0800

    Input Search Box finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
-  console.log("What am I? -- SERVER/CLIENT");
+export default async function Home({
+  searchParams,
+}: {
+  // Next 15: searchParams is a Promise
+  searchParams?: Promise<Record<string, string | string[] | undefined>>;
+}) {
+  const sp = await searchParams;
+
+  // pull ?query=... from the URL safely
+  const q = sp?.query;
+  const query = Array.isArray(q) ? q[0] : q ?? "";
+
   return (
-    <>
-      <h1 className="heading">Watch me go</h1>
-    </>
+    <section className="pink_container pattern">
+      <h1 className="heading">
+        Pitch Your Startup, <br /> Connect with Entrepreneurs
+      </h1>
+
+      <p className="sub-heading bg-amber-400 !max-w-3xl">
+        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+      </p>
+
+      <SearchForm query={query} />
+    </section>
   );
 }
