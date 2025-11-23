a99616d576c35857120f516150988d71bf780a98
ClaraXinn <claraxin8@gmail.com>
2025-11-24 00:44:10 +0800

    Sanity Set Up done

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,23 +1,23 @@
 import { formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Link from "next/link";
-import React from "react";
 import Image from "next/image";
+
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const {
     _createdAt,
     views,
     author: { _id: authorId, name },
     title,
-    category,
     _id,
     image,
     description,
   } = post;
+
   return (
-    <li className="startup-card group">
+    <li className="startup-card group w-full max-w-[360px]">
       <div className="flex-between">
-        <p className="startup-card_date">{formatDate(post._createdAt)}</p>
+        <p className="startup-card_date">{formatDate(_createdAt)}</p>
         <div className="flex gap-1.5">
           <EyeIcon className="size-6 text-primary" />
           <span className="text-16-medium">{views}</span>
@@ -31,24 +31,34 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
           </Link>
 
           <Link href={`/startup/${_id}`}>
-            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+            <h3 className="text-26-semibold line-clamp-2">{title}</h3>
           </Link>
         </div>
+
         <Link href={`/user/${authorId}`}>
-            <Image src={"https://placehold.co/48x48"} alt={"placeholder"} 
-            width={48} height={48} className="rounded-full"/>
+          <Image
+            src="https://placehold.co/48x48"
+            alt="placeholder"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
         </Link>
-
-
       </div>
 
-      <Link href={'/startup/${_id}'}>
-            <p className="startup-card-desc">
-                {description}
-            </p>
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card-desc line-clamp-2">{description}</p>
 
-            <img src={image} alt="" />
-        </Link>
+        {/* YouTube Style Thumbnail */}
+        <div className="relative w-full aspect-video mt-3">
+          <Image
+            src={image}
+            alt={title}
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </Link>
     </li>
   );
 };
