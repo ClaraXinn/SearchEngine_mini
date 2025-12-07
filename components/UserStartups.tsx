9efb0f8a8bc4d8df100b0a1703067480c73a728f
ClaraXinn <claraxin8@gmail.com>
2025-12-07 18:07:07 +0800

    next config update

--- components/UserStartups.tsx
+++ components/UserStartups.tsx
@@ -1,5 +1,5 @@
 import { client } from "@/sanity/lib/client";
-import { STARTUPS_BY_AUTHOR_QUERY } from "@/sanity/lib/queries";
+
 import React from "react";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
